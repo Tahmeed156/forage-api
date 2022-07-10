0a32a3f9ab56c7adabfaade3dc9a1e5f06d6b321
Tahmeed Tarek <tahmeedtarek@gmail.com>
2022-07-10 17:52:06 +0600

    Add models to django-admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from api.models import User, Paper, Project, ProjectList, ProjectCollaborator, ProjectPaper
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Paper)
+admin.site.register(Project)
+admin.site.register(ProjectCollaborator)
+admin.site.register(ProjectList)
+admin.site.register(ProjectPaper)
+
