559ff730dc2935cad5e979643ab3994ac41873f3
Tahmeed Tarek <tahmeedtarek@gmail.com>
2022-08-12 10:59:47 +0600

    Add model for notes

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from forage import settings
@@ -108,6 +109,24 @@ class ProjectPaper(models.Model):
 
     def __str__(self):
         return f"{self.id}-{self.paper.name[:20]}...-{self.list.name}"
+
+
+class Note(models.Model):
+    project_paper = models.OneToOneField(
+        ProjectPaper,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='note'
+    )
+    creator_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    # NOTE: visibility choices = ['Private', 'Public']
+    text = models.TextField(default='', blank=True)
+    visibility = models.CharField(max_length=64, default='Private', blank=True)
+    last_modified = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.last_modified = datetime.now()
+        super(Note, self).save(*args, **kwargs)
 
 
 class Task(models.Model):
